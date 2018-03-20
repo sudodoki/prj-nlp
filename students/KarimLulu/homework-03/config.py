81ab55ce42c082d0c441fb68bd176c34cad25d5d
Karim Lulu <klulu@digamma.ai>
2018-03-20 03:04:55 +0200

    hw#3, added SAX

--- students/KarimLulu/homework-03/config.py
+++ students/KarimLulu/homework-03/config.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
-log_format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+log_fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+date_fmt="%Y-%m-%d %H:%M:%S"
 
 root_dir = Path(__file__).resolve().parent
 posts_dir = root_dir / "posts"
+
+wiki_filename = "bgwiktionary-20180301-pages-articles-multistream.xml.bz2"
+wiki_output = "wiktionary.txt"
 
 def init_dir(folder):
     Path.mkdir(folder, parents=True, exist_ok=True)
