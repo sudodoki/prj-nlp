64a4d3c8cff02feea6a3fd8c2775fbabea8d9208
Viktor Kovryzhkin <vik.kovrizhkin@gmail.com>
2018-03-22 11:32:09 +0200

    task 3 doc in spark

--- students/vikua/task_3/3_common_crawl_spark.py
+++ students/vikua/task_3/3_common_crawl_spark.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 from readability import Document
 from langdetect import detect
@@ -7,17 +6,21 @@ from langdetect.lang_detect_exception import LangDetectException
 
 from pyspark.sql.functions import udf, col
 from pyspark.sql.types import ArrayType, StringType
-
+from pyspark.ml.feature import Tokenizer
 
 TAG_RE = re.compile(r'<[^>]+>')
 
 
 def extract_title_and_summary(content):
+    """ Apply readability to extract title and summary of the page
+    """
     doc = Document(content)
     return [doc.title(), doc.summary()]
 
 
 def clean_summary(summary):
+    """ Delete html tags, which still present after readability processing.
+    """
     if summary:
         s = TAG_RE.sub('', summary)
         s = s.replace('\n', ' ').replace('\r', '')
@@ -28,6 +31,8 @@ def clean_summary(summary):
 
 
 def get_lang(title):
+    """ Detect language from title
+    """
     try:
         lang = detect(title)
     except LangDetectException:
@@ -35,9 +40,17 @@ def get_lang(title):
     return lang
 
 
+def filter_empty_and_join(words):
+    """ Filters empty tokens and joins to single string
+    """
+    w = [x for x in words if x.strip() != '']
+    return ' '.join(w)
+
+
 extract_title_and_summary_udf = udf(extract_title_and_summary, ArrayType(StringType()))
 clean_summary_udf = udf(clean_summary, StringType())
 get_lang_udf = udf(get_lang, StringType())
+filter_empty_udf = udf(filter_empty_and_join, StringType())
 
 df = spark.read.option("header", "true").csv("raw.csv")
 
@@ -48,4 +61,10 @@ df = df.withColumn('title_sum', extract_title_and_summary_udf(col("content")))\
        .withColumn('lang', get_lang_udf(col('title')))\
        .select("domain", "lang", "title", "summary")
 
-df.write.option("header", "true").csv("preprocessed.csv")
+tokenizer = Tokenizer(inputCol='summary', outputCol='summary_words')
+df = tokenizer.transform(df)\
+       .withColumn('words', filter_empty_udf(col('summary_words')))\
+       .select('domain', 'lang', 'title', 'words')
+
+
+df.write.csv("preprocessed.csv")
