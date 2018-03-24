feae45a75857523dc6f09ad36e11566366b5e058
bmarchenko <bogdan.marko@gmail.com>
2018-03-24 15:43:32 +0200

    fixes to task 3 after explanations

--- students/BohdanMarchenko/03-nlp-data/Annotation/nucle.py
+++ students/BohdanMarchenko/03-nlp-data/Annotation/nucle.py
@@ -1,8 +1,25 @@
 from bs4 import BeautifulSoup
 import statistics
 
-def process():
-    TEAMS = ['a', 'b', 'c']
+TEAMS = ['a', 'b', 'c']
+
+def agreed_2():
+    #at least 2 agreed
+    docs = []
+    for team in TEAMS:
+        with open("conll14st-test-data/alt/alternative-team{}.sgml".format(team), 'r') as fl:
+            soup = BeautifulSoup(fl, 'lxml')
+            for i in range(1, 51):
+                doc = soup.find('doc', {'nid': str(i)})
+                if doc:
+                    docs += doc.findAll('mistake')
+    agreement = (len(docs) - len(set(docs)))/(len(docs)/3)
+    print("At least 2 agreed. AGREEMENT RATE: {}%".format(round(agreement * 100), 3))
+    # OUTPUT:
+    # AGREEMENT RATE: 31%
+
+def all_agreed():
+    # all 3  agreed
     docs = []
     agreement = []
     for team in TEAMS:
@@ -19,12 +36,12 @@ def process():
 
     for doc in range(50):
         dc_mistakes = [set(docs[team][doc]) for team in range(len(TEAMS))]
-        agreement.append(len(set.intersection(*dc_mistakes))/max([len(i) for i in dc_mistakes]))
-    print("AGREEMENT RATE: {}%".format(round(statistics.mean(agreement)*100, 2)))
+        agreement.append(len(set.intersection(*dc_mistakes))/(sum([len(i) for i in dc_mistakes])/3))
+    print("All agreed. AGREEMENT RATE: {}%".format(round(statistics.mean(agreement)*100, 2)))
     # OUTPUT:
-    #AGREEMENT RATE: 1.19%
-
+    #AGREEMENT RATE: 1.72%
 
 if __name__ == "__main__":
-    process()
+    agreed_2()
+    all_agreed()
 
--- students/BohdanMarchenko/03-nlp-data/DataAcquisition/3-sparql-query.md
+++ students/BohdanMarchenko/03-nlp-data/DataAcquisition/3-sparql-query.md
@@ -1,11 +1,8 @@
 3. Write a query to collect all relations from dbpedia for every individual person listed in it - requires SPARQL
 
 
-PREFIX dbo: <http://dbpedia.org/ontology/>
 PREFIX dbr: <http://dbpedia.org/resource/>
-
-SELECT DISTINCT ?name ?relation
-WHERE {
-?person dbo:relation ?relation .
-?person rdfs:label ?name FILTER (lang(?name) = "en").
+select * { 
+  values ?person { dbr:Johnny_Cash }
+  ?person ?p ?o 
 }
