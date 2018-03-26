474496bfc35ab0f3479c30804018b1c252b9ed3d
karim.lulu <karimlulu@karim.lulu>
2018-03-26 20:29:32 +0300

    hw#4, added ground truth data fetching

---
+++ students/KarimLulu/homework-04/config.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+log_fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+date_fmt="%Y-%m-%d %H:%M:%S"
+
+root_dir = Path(__file__).resolve().parent
+data_dir = root_dir / "data"
+db_filename = "db.json"
+
+# SPARQL
+ENDPOINT = "http://dbpedia.org/sparql"
+QUERY = """
+PREFIX dbo: <http://dbpedia.org/ontology/>
+PREFIX dbpedia: <http://dbpedia.org/resource/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX dbr: <http://dbpedia.org/resource/>
+PREFIX purl: <http://purl.org/linguistics/gold/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+SELECT DISTINCT SAMPLE(STR(?name)) as ?name SAMPLE(STR(?painting)) as ?painting
+WHERE {
+   ?person a dbo:Person ;
+          purl:hypernym dbr:Painter ;
+          dbo:birthPlace ?city ;
+          foaf:name ?name .
+   ?city dbo:country ?country .
+   ?picture dbo:author ?person ;
+            foaf:name ?painting .
+FILTER (lang(?painting)="en")
+FILTER (lang(?name)="en")
+}
+GROUP BY ?person ?picture
+ORDER BY ?name ?painting
+"""
+
+def init_dir(folder):
+    Path.mkdir(folder, parents=True, exist_ok=True)
--- students/KarimLulu/homework-04/system.py
+++ students/KarimLulu/homework-04/system.py
@@ -1,17 +1,58 @@
-query = """
-PREFIX dbo: <http://dbpedia.org/ontology/>
-PREFIX db: <http://dbpedia.org/page/>
-PREFIX foaf:  <http://xmlns.com/foaf/0.1/>
-PREFIX dbr:    <http://dbpedia.org/resource/>
-PREFIX purl: <http://purl.org/linguistics/gold/>
+import sys
+import os
+from SPARQLWrapper import SPARQLWrapper, JSON
+from collections import defaultdict
+import json
 
-SELECT ?name, ?picture
-WHERE {
-   ?person a dbo:Person ;
-          purl:hypernym dbr:Painter ;
-          dbo:birthName ?name ;
-          dbo:birthPlace ?city ;
-          dbo:birthDate ?birth .
-   ?city dbo:country ?country .
-   ?picture dbo:author ?person .
-"""
+from config import (data_dir, init_dir, db_filename, log_fmt, date_fmt,
+                    ENDPOINT, QUERY)
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format=log_fmt,
+                    datefmt=date_fmt)
+logger = logging.getLogger(__name__)
+
+PIPELINE = {"get_data": {}}
+DISABLE = []#"get_data"]
+RULES = ["rule_1"]
+
+def get_data():
+    logger.info("Fetching the ground truth data")
+    sparql = SPARQLWrapper(ENDPOINT)
+    sparql.setReturnFormat(JSON)
+    sparql.setQuery(QUERY)
+    results = sparql.query().convert()
+    logger.info(f'Columns: {results["head"]["vars"]}')
+    logger.info(f'Fetched {len(results["results"]["bindings"])} records')
+    data = defaultdict(list)
+    for result in results["results"]["bindings"]:
+        artist = result["name"]["value"]
+        painting = result["painting"]["value"]
+        data[artist].append(painting)
+    with (data_dir / db_filename).open("w+") as f:
+        json.dump(data, f, indent=4)
+    return data
+
+def train_test():
+    pass
+
+def rule_1():
+    pass
+
+def apply_rules():
+    pass
+
+def metric():
+    pass
+
+def main():
+    init_dir(data_dir)
+    for pipe, attrs in PIPELINE.items():
+        if pipe not in DISABLE:
+            output = globals()[pipe](**attrs)
+    return 0
+
+if __name__ == "__main__":
+    code = main()
+    sys.exit(code)
