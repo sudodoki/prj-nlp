d335467659437f4260d76dd721cc7de96d30f6d5
juliamakogon <juogon@gmail.com>
2018-03-30 22:52:54 +0300

    Prepare the database

--- students/juliamakogon/task_04/prepare_db.py
+++ students/juliamakogon/task_04/prepare_db.py
@@ -10,7 +10,7 @@ import sys
 def prepareMovieTitle(s):
     film = s
     end = len(film) - 4
-    year = 0
+    year = '0'
     match = re.search(r'\((\d{4}).* film\)', film)
     if match:
         year = match[1]
@@ -39,23 +39,33 @@ def getTextFromHtml(path, name, url):
         text = soup_wiki.text.strip()
         if text:
             with open(fname, 'w', encoding='utf-8') as f:
-                f.write(soup_wiki.text)
+                f.write(text)
     except Exception as e:
         print("Failed to save a page: {}.\nException: {}".format(url, e))    
 
 
 def prepareDbPos(path, inputdb_html, outputdb_csv):
-    with open(inputdb_html, 'r') as fp:
+    with open(inputdb_html, 'r', encoding='utf-8') as fp:
         soup = BeautifulSoup(fp)
 
     table = soup.find('table')
     rows = table.find_all('tr')
     I_MOVIE = 1 # movieLabel
     I_WIKI = 2 # wiki
+    I_DATE = 3
+    I_YEAR_CAT = 4
     with open(outputdb_csv, 'w', encoding="utf-8") as f:
         for i in range(1, min(len(rows), 200000)) :
             td = rows[i].find_all("td")
             (film, year) = prepareMovieTitle(td[I_MOVIE].get_text())
+            if year == '0':
+                match = re.findall(r'\d{4}', td[I_DATE].get_text())
+                if match:
+                    year = match[0]
+            if year =='0':
+                match = re.findall(r'\d{4}', td[I_YEAR_CAT].get_text())
+                if match:
+                    year = match[0]               
             f.write('{}\t{}\n'.format(film, year))
             getTextFromHtml(path, filenameForMovie(td[I_MOVIE].get_text()), td[I_WIKI].get_text())  
 
