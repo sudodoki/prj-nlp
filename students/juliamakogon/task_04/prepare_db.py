405747c5b2a68755d67231d9ec24680640a99225
juliamakogon <juogon@gmail.com>
2018-03-30 22:05:11 +0300

    Prepare For Cinderella

--- students/juliamakogon/task_04/prepare_db.py
+++ students/juliamakogon/task_04/prepare_db.py
@@ -36,9 +36,10 @@ def getTextFromHtml(path, name, url):
         r.raise_for_status()
         summary = Document(r.content).summary()
         soup_wiki = BeautifulSoup(summary)
-        with open(fname, 'w', encoding='utf-8') as f:
-            f.write(soup_wiki.text)
-
+        text = soup_wiki.text.strip()
+        if text:
+            with open(fname, 'w', encoding='utf-8') as f:
+                f.write(soup_wiki.text)
     except Exception as e:
         print("Failed to save a page: {}.\nException: {}".format(url, e))    
 
@@ -49,18 +50,13 @@ def prepareDbPos(path, inputdb_html, outputdb_csv):
 
     table = soup.find('table')
     rows = table.find_all('tr')
-    I_MOVIE = 2 # movieLabel
-    I_CAT = 3 # catLabel
-    I_WIKI = 4 # wiki
+    I_MOVIE = 1 # movieLabel
+    I_WIKI = 2 # wiki
     with open(outputdb_csv, 'w', encoding="utf-8") as f:
         for i in range(1, min(len(rows), 200000)) :
             td = rows[i].find_all("td")
-            fairytale = td[I_CAT].get_text()
-            match = re.findall(r'"Films based on (.+)"', fairytale)
-            if match:
-                fairytale = match[0]
             (film, year) = prepareMovieTitle(td[I_MOVIE].get_text())
-            f.write('"{}"\t{}\t"{}"\n'.format(film, year, fairytale))
+            f.write('{}\t{}\n'.format(film, year))
             getTextFromHtml(path, filenameForMovie(td[I_MOVIE].get_text()), td[I_WIKI].get_text())  
 
 
@@ -76,12 +72,12 @@ def prepareNeg(path, input_html):
         td = rows[i].find_all("td")
         getTextFromHtml(path, filenameForMovie(td[I_MOVIE].get_text()), td[I_WIKI].get_text())    
 
-path = sys.argv[1]
+#path = sys.argv[1]
 #path = r'C:\work\jul\prj-nlp\students\juliamakogon\task_04\wiki_testset'
+path = r'C:\work\jul\prj-nlp\students\juliamakogon\task_04\cind_test'
 pos_path = os.path.join(path, r'pos')
 neg_path = os.path.join(path, r'neg')
 print(pos_path)
 print(neg_path)
-prepareDbPos(pos_path, "fairytale_sparql.html", "fairytale_db.csv")
+prepareDbPos(pos_path, "cinderella_sparql.html", "cinderella_db.csv")
 prepareNeg(neg_path, "hippi_sparql.html")
- 
