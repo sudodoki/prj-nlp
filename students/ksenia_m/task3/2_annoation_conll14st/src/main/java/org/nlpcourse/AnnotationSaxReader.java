c9e8fd5ecdfc7a334a01d4bf092c5b799e8d636f
Kseniia Mullomukhametova <kseniia_mullomukhametova@epam.com>
2018-04-02 11:23:31 +0300

    added fixes

--- students/ksenia_m/task3/2_annoation_conll14st/src/main/java/org/nlpcourse/AnnotationSaxReader.java
+++ students/ksenia_m/task3/2_annoation_conll14st/src/main/java/org/nlpcourse/AnnotationSaxReader.java
@@ -1,51 +1,34 @@
 package org.nlpcourse;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.SequenceInputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.nlpcourse.model.Mistake;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-public class AnnotationReader {
-    private String sourcePath;
+public class AnnotationSaxReader extends AbstractXmlParser {
 
     private Set<Mistake> mistakes = new LinkedHashSet<>();
+
     private Mistake current;
 
-    public AnnotationReader(String sourcePath) {
-        this.sourcePath = sourcePath;
+    public AnnotationSaxReader(String sourcePath) {
+        super(sourcePath);
     }
 
-    public Set<Mistake> parseAndGetMistakes() throws ParserConfigurationException, SAXException, IOException {
+    public void parse() throws ParserConfigurationException, SAXException, IOException {
         newParser().parse(getWrapperStream(sourcePath), new SAXHandler());
-        return mistakes;
     }
 
     private SAXParser newParser() throws ParserConfigurationException, SAXException {
         return SAXParserFactory.newInstance().newSAXParser();
-    }
-
-    private SequenceInputStream getWrapperStream(String sourcePath) throws FileNotFoundException {
-        return new SequenceInputStream(Collections.enumeration(Arrays.asList(
-                new ByteArrayInputStream("<root>".getBytes()),
-                new FileInputStream(sourcePath),
-                new ByteArrayInputStream("</root>".getBytes())
-        )));
     }
 
     private class SAXHandler extends DefaultHandler {
@@ -57,10 +40,8 @@ public class AnnotationReader {
                 throws SAXException {
             if ("MISTAKE".equalsIgnoreCase(qName)) {
                 current = new Mistake();
-               current.setStartToken(Integer.valueOf(atts.getValue("start_token")));
-               current.setEndToken(Integer.valueOf(atts.getValue("end_token")));
-               current.setNid(Integer.valueOf(atts.getValue("nid")));
-               current.setPid(Integer.valueOf(atts.getValue("pid")));
+               current.setStartOff(Integer.valueOf(atts.getValue("start_token")));
+               current.setEndOff(Integer.valueOf(atts.getValue("end_token")));
             }
             currentQName = qName;
         }
