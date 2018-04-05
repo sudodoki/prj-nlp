571455f9fe60a4ee52ce06cbbb3cf170d26a2ba1
olegValensa <olegvalensa@gmail.com>
2018-04-06 00:55:30 +0300

    hw5 v1

--- students/oleg_m/bow_rozetka/rozetka/rozetka/settings.py
+++ students/oleg_m/bow_rozetka/rozetka/rozetka/settings.py
@@ -16,10 +16,12 @@ NEWSPIDER_MODULE = 'rozetka.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'rozetka (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:59.0) Gecko/20100101 Firefox/59.0'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
+
+FEED_EXPORT_FIELDS = ['Stars', 'Response', 'Normal_text', 'Pro', 'Con']
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,13 +29,13 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
