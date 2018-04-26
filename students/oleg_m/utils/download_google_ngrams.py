e5f2b82d50fa633fb1a1a7366a656bedb53910bf
olegValensa <olegvalensa@gmail.com>
2018-04-27 00:55:43 +0300

    hw8 delete hw7

--- students/oleg_m/utils/download_google_ngrams.py
+++
@@ -1,8 +0,0 @@
-from google_ngram_downloader import readline_google_store
-
-fname, url, records = next(readline_google_store(ngram_len=2))
-fname
-'googlebooks-eng-all-5gram-20120701-0.gz'
-url
-'http://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-5gram-20120701-0.gz'
-next(records)
